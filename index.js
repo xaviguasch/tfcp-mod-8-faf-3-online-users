1789217a5bd3dae83fe8a29030a69f607c6c99b6
xaviguasch <xaviguasch@gmail.com>
2020-08-20 10:50:02 +0200

    added starter files

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+    1. 
+    
+    Load users from users.json (originally from https://jsonplaceholder.typicode.com/users)
+    
+    Create a simple component that displays the user's username 
+        -Alongside a green circle
+        -The username and circle should be in individual divs
+        -Both should be vertically and horizontally centered
+        -The component should only be wide enough to Fit its Content (and some padding)
+*/
