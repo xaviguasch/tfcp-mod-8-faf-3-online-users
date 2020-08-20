b3f931085f00559d1e3edc4dcdbbd67c87287098
xaviguasch <xaviguasch@gmail.com>
2020-08-20 12:02:55 +0200

    solved first challenge

--- index.js
+++ index.js
@@ -9,3 +9,21 @@
         -Both should be vertically and horizontally centered
         -The component should only be wide enough to Fit its Content (and some padding)
 */
+
+async function getUsers() {
+  let response = await fetch('users.json')
+  let users = await response.json()
+  return users
+}
+
+getUsers().then((users) => {
+  let sampleUser = users[0]
+  let userDiv = `
+      <div class="my-online-user">
+          <div class="user-username">${sampleUser.username}</div>
+          <div class="user-online"></div>
+      </div>
+  `
+
+  document.body.innerHTML = userDiv
+})
